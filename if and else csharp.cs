07f3a80c48284e8c18ab991b24a1dbdddb6192b0
Felipe De Souza <146121300+FelipeSoouza@users.noreply.github.com>
2024-06-24 15:23:57 -0300

    Add files via upload

---
+++ if and else csharp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studyCsharp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("coloque um numero inteiro");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0) 
+            {
+                Console.WriteLine("par!");
+            }
+            else
+            {
+                Console.WriteLine("Impar!");
+            }
+        }
+    }
+}
